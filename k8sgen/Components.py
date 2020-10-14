d1f9109e603a32476e80c16692abff1d70d3e21b
John Carter <jfcarter2358@gmail.com>
2020-10-14 13:43:44 -0500

    Apply presenters to classes

--- k8sgen/Components.py
+++ k8sgen/Components.py
@@ -3,6 +3,16 @@ import yaml
 from k8sgen import utils
 import pkgutil
 
+def str_presenter(dumper, data):
+    if len(data.splitlines()) > 1:  # check for multiline string
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+def represent_none(self, _):
+    return self.represent_scalar('tag:yaml.org,2002:null', '')
+
+yaml.add_representer(str, str_presenter)
+yaml.add_representer(type(None), represent_none)
 '''
 Kubernetes VolumeMount component for use in API resources
 
